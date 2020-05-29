13c75f0da693b79b2cef0be7650cc8c002345bab
Junior Navarro <cjuniordse@Juniors-Mac-mini.local>
2020-05-28 22:48:55 -0500

    Did some styles

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,19 +1,42 @@
 import React from "react";
-
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
  const NasaCard = (props) => {
+     console.log("props", props);
+
+     const { photo } = props;
+
+//     return (
+//         <div>
+//             <div className="header">
+//                 <CardTitle>{photo.title}</CardTitle>
+//                 <p>{photo.date}</p>
+//                 <img 
+//                     className="image" alt="Nasa Photo" src={photo.url}/>
+                
+//                 <p>{photo.explanation}</p>
+                
+//                 <p>{photo.copyright}</p>
+//             </div>    
+//         </div>
+//     )
+// };
+
     return (
-        <div className="nasa-card" >
-        <div className="header">
-            <h1>{props.title}</h1>
-            <h3>{props.date}</h3>
-        </div>
-            <img src={props.imgUrl}alt="Nasa" />
-             <p className="explanation">{props.explanation}</p>
-             <h3 className="copyright">Copyright: {props.copyright}</h3>
+        <div>
+            <Card>
+                <CardBody>
+                    <CardTitle className="title">{photo.title}</CardTitle>
+                    <CardSubtitle className="date">{photo.date}</CardSubtitle>
+                </CardBody>                
+                    <img width="80%" src={photo.url} alt="Nasa Photo"/>
+                <CardBody>
+                    <CardText className="explanation">{photo.explanation}</CardText>
+                    <CardText className="copyright">{photo.copyright}</CardText>
+                </CardBody>
+            </Card>
         </div>
     )
-};
-
+ };
 export default NasaCard;
   
